5bb2a0c84c66675493031b6273d8cce064a4edbf
cmcdgit <cmcdgit@gmail.com>
2024-05-14 15:47:03 +0100

    Hono basic setup implemented

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
+const isProtectedRoute = createRouteMatcher(["/"]); // todo: api route can be protected by adding this, "/api(.*)"]);
 
 export default clerkMiddleware((auth, request) => {
   if (isProtectedRoute(request)) {
